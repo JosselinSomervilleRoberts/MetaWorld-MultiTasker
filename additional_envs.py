100a9c95c954710812683c694fa5b03f8c6ce271
JosselinSomervilleRoberts <josselin.somerville@polytechnique.edu>
2023-05-23 12:17:17 -0700

    Added MTFlexible

--- additional_envs.py
+++ additional_envs.py
@@ -7,8 +7,16 @@ from metaworld.envs.mujoco.sawyer_xyz.v2 import (
     SawyerPickPlaceEnvV2,
     SawyerPushEnvV2,
     SawyerReachEnvV2,
+    SawyerDoorEnvV2,
+    SawyerDrawerOpenEnvV2,
+    SawyerDrawerCloseEnvV2,
+    SawyerButtonPressTopdownEnvV2,
+    SawyerPegInsertionSideEnvV2,
+    SawyerWindowOpenEnvV2,
+    SawyerWindowCloseEnvV2,
 )
 
+# =========== Custom MT3 (3 first tasks of MT10) =========== #
 
 MT3_V2 = OrderedDict(
     (('reach-v2', SawyerReachEnvV2),
@@ -32,3 +40,39 @@ class MT3(metaworld.Benchmark):
                                         metaworld._MT_OVERRIDE,
                                         seed=seed)
         self._test_tasks = []
+
+# ========================================================= #
+
+
+
+# =========== Custom MTFlexible (N first tasks of MT10) =========== #
+
+MTFLEXIBLE_V2 = OrderedDict(
+    (('reach-v2', SawyerReachEnvV2),
+     ('push-v2', SawyerPushEnvV2),
+     ('pick-place-v2', SawyerPickPlaceEnvV2),
+     ('door-open-v2', SawyerDoorEnvV2),
+     ('drawer-open-v2', SawyerDrawerOpenEnvV2),
+     ('drawer-close-v2', SawyerDrawerCloseEnvV2),
+     ('button-press-topdown-v2', SawyerButtonPressTopdownEnvV2),
+     ('peg-insert-side-v2', SawyerPegInsertionSideEnvV2),
+     ('window-open-v2', SawyerWindowOpenEnvV2),
+     ('window-close-v2', SawyerWindowCloseEnvV2)),)
+     
+class MTFlexible(metaworld.Benchmark):
+    def __init__(self, n: int, seed=None):
+        super().__init__()
+        assert n >= 2, "n must be >= 2, otherwise it is not multi-task"
+        assert n <= 10, "n must be <= 10, as it currently uses MT10"
+        # Keep the first n tasks of MTN_V2
+        self._train_classes = OrderedDict(list(MTFLEXIBLE_V2.items())[:n])
+        self._test_classes = OrderedDict()
+        train_kwargs = {key: dict(args=[],
+                                    kwargs={'task_id': list(_env_dict.ALL_V2_ENVIRONMENTS.keys()).index(key)})
+                        for key, _ in self._train_classes.items()}
+        self._train_tasks = metaworld._make_tasks(self._train_classes, train_kwargs,
+                                        metaworld._MT_OVERRIDE,
+                                        seed=seed)
+        self._test_tasks = [] 
+
+# ========================================================= #
